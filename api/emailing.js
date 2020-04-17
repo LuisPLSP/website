dbc291cb0b40732d35b97be438a76ec9c3da88d4
Luis Peixoto <luisplsp@gmail.com>
2020-04-17 12:35:01 +0100

    relocate env and serverless funct

---
+++ api/emailing.js
@@ -0,0 +1,29 @@
+module.exports = (req, res) => {
+    const data = req.body;
+
+  const output = `
+    <p>You have a new contact request!</p>
+    <h3>Contact details</h3>
+    <p>Email: ${data.email}</p>
+    <h3>Message</h3>
+    <p>${data.message}</p>
+    `;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  let info = transporter.sendMail({
+      from: '"S2P Website" <stepstopodium@gmail.com>', // sender address
+      to: "stepstopodium@gmail.com", // list of receivers
+      subject: `${data.subject}`, // Subject line
+      html: output // plain text body
+    })
+    .then(() => {
+      res.send({msg: "Your email has been sent. We'll get back to you has soon as possible!"});
+      });
+  }
