60df48f8c3cd1acd7112953fb18fb56c97c2a911
Luis Peixoto <luisplsp@gmail.com>
2022-02-14 10:41:16 +0000

    remove rear shock course in Porto 27th march and adjust lisbon course to a single day

--- src/components/Allcourses.vue
+++ src/components/Allcourses.vue
@@ -48,22 +48,27 @@
           class="module mb-3"
         >
           <b-card
+            style="position: relative;"
             class="shadow-lg card"
-            img-src="https://firebasestorage.googleapis.com/v0/b/steps-to-podium-website-d4f43.appspot.com/o/courses%2FShock-card.jpg?alt=media&token=fa899d97-6966-41ee-bd05-b69c91c86f6e"
-            img-alt="rear shock course"
+            img-src="https://firebasestorage.googleapis.com/v0/b/steps-to-podium-website-d4f43.appspot.com/o/courses%2FFrontForks-card.jpg?alt=media&token=57aa9485-d3c2-47b3-9004-a1f1e6cbbcba"
+            img-alt="Front fork course"
             overlay
           >
             <div class="overlay-text">
-              <h2>{{ $t("courses.shockcourses") }}</h2>
+              <h2>{{ $t("courses.forkscourses") }}</h2>
               <h3>
-                {{ $t("courses.shockdate1") }} |
-                {{ $t("courses.shocklocation1") }}
+                {{ $t("courses.forksdate2") }} |
+                {{ $t("courses.forkslocation3") }}
               </h3>
             </div>
-            <router-link to="/rearshockcourse">
+            <router-link to="/frontforkscourse">
               <b-button
                 v-on:click="
-                  cardClicked({ time: 'one', price: 'regular', location: 'one' })
+                  cardClicked({
+                    time: 'zero',
+                    price: 'higher',
+                    location: 'three'
+                  })
                 "
                 class="btn-to-course"
                 >{{ $t("courses.watchmore") }}</b-button
@@ -94,52 +99,13 @@
               <b-button
                 v-on:click="
                   cardClicked({
-                    time: 'zero',
+                    time: 'one',
                     price: 'high',
                     location: 'three'
                   })
                 "
                 class="btn-to-course"
                 >{{ $t("courses.watchmore") }}</b-button
-              >
-            </router-link>
-          </b-card>
-        </b-col>
-        <!-- </b-row>
-      <b-row class="mb-md-5" align-h="around"> -->
-        <!-- <b-col lg="5" data-aos="fade-up" data-aos-duration="300" class="module mb-3">
-        </b-col> -->
-        <b-col
-          lg="5"
-          data-aos="fade-up"
-          data-aos-duration="200"
-          class="module mb-3"
-        >
-          <b-card
-            style="position: relative;"
-            class="shadow-lg card"
-            img-src="https://firebasestorage.googleapis.com/v0/b/steps-to-podium-website-d4f43.appspot.com/o/courses%2FFrontForks-card.jpg?alt=media&token=57aa9485-d3c2-47b3-9004-a1f1e6cbbcba"
-            img-alt="Front fork course"
-            overlay
-          >
-            <div class="overlay-text">
-              <h2>{{ $t("courses.forkscourses") }}</h2>
-              <h3>
-                {{ $t("courses.forksdate3") }} |
-                {{ $t("courses.forkslocation3") }}
-              </h3>
-            </div>
-            <router-link to="/frontforkscourse">
-              <b-button
-                v-on:click="
-                  cardClicked({
-                    time: 'zero',
-                    price: 'higher',
-                    location: 'three'
-                  })
-                "
-                class="btn-to-course"
-                >{{ $t("courses.comingsoon") }}</b-button
               >
             </router-link>
           </b-card>
